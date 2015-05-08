59ebd536fd1a39c1b4f8128d78cd85adfd051f45
Pim De Witte <pim@whitespell.com>
2015-05-08 08:39:56 -0400

    removed unnecessary wwadewitte

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 #remove the directories
-ssh $USER@peakapi.whitespell.com "sudo rm -rf /usr/share/peak-api";
+ssh peakapi.whitespell.com "sudo rm -rf /usr/share/peak-api";
 
 #create the right directories
-ssh $USER@peakapi.whitespell.com "sudo mkdir /usr/share/peak-api && sudo chmod -R 777 /usr/share/peak-api";
-ssh $USER@peakapi.whitespell.com "sudo mkdir /usr/share/peak-api/bin && sudo chmod -R 777 /usr/share/peak-api/bin";
-ssh $USER@peakapi.whitespell.com "sudo mkdir /usr/share/peak-api/lib && sudo chmod -R 777 /usr/share/peak-api/lib";
+ssh peakapi.whitespell.com "sudo mkdir /usr/share/peak-api && sudo chmod -R 777 /usr/share/peak-api";
+ssh peakapi.whitespell.com "sudo mkdir /usr/share/peak-api/bin && sudo chmod -R 777 /usr/share/peak-api/bin";
+ssh peakapi.whitespell.com "sudo mkdir /usr/share/peak-api/lib && sudo chmod -R 777 /usr/share/peak-api/lib";
 
 #//todo make it zip based
 #place the right content in the directories
-scp -r bin/* $USER@peakapi.whitespell.com:/usr/share/peak-api/bin
-scp -r lib/* $USER@peakapi.whitespell.com:/usr/share/peak-api/lib
-scp run.sh $USER@peakapi.whitespell.com:/usr/share/peak-api
-scp config.prop $USER@peakapi.whitespell.com:/usr/share/peak-api
-scp build.sh $USER@peakapi.whitespell.com:/usr/share/peak-api
+scp -r bin/* peakapi.whitespell.com:/usr/share/peak-api/bin
+scp -r lib/* peakapi.whitespell.com:/usr/share/peak-api/lib
+scp run.sh peakapi.whitespell.com:/usr/share/peak-api
+scp config.prop peakapi.whitespell.com:/usr/share/peak-api
+scp build.sh peakapi.whitespell.com:/usr/share/peak-api
 
 #kill current api, and nohup the new version
-ssh $USER@peakapi.whitespell.com "cd /usr/share/peak-api && pkill java && nohup bash run.sh bin &";
+ssh peakapi.whitespell.com "cd /usr/share/peak-api && pkill java && nohup bash run.sh bin &";
