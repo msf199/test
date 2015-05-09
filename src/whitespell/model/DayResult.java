2fd2bb574f29932a25dc0c90cd19c8b7e01a17b0
Pim De Witte <pim@whitespell.com>
2015-05-09 00:01:03 -0400

    added sign ups

---
+++ src/whitespell/model/DayResult.java
@@ -0,0 +1,19 @@
+package whitespell.model;
+
+/**
+ * @author Pim de Witte(wwadewitte), Whitespell LLC
+ *         5/8/15
+ *         whitespell.model
+ */
+public class DayResult {
+    String day = null;
+    int data = -1;
+
+    public DayResult(String day, int data) {
+        this.day = day;
+        this.data = data;
+    }
+
+
+
+}
