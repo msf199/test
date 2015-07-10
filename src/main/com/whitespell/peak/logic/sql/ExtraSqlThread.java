099538dfd2b25d3e6348d0e5954f61d4a6513eea
Pim De Witte <pim@whitespell.com>
2015-07-10 16:01:49 -0400

    changes to naming

---
+++ src/main/com/whitespell/peak/logic/sql/ExtraSqlThread.java
@@ -0,0 +1,9 @@
+package main.com.whitespell.peak.logic.sql;
+
+/**
+ * @author Pim de Witte(wwadewitte), Whitespell LLC
+ *         7/10/15
+ *         main.com.whitespell.peak.logic.sql
+ */
+public class ExtraSqlThread {
+}
