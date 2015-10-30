79543b1a080f68714f4a483b834aeee9070f6cf2
Cory McAn <rulerfenrir@aim.com>
2015-10-30 13:25:16 -0400

    make all content free and accessible

--- src/main/com/whitespell/peak/logic/ContentWrapper.java
+++ src/main/com/whitespell/peak/logic/ContentWrapper.java
@@ -207,6 +207,22 @@ public class ContentWrapper {
 
         int currentContentId = tempContent.getContentId();
         try {
+
+            //todo(cmcan) REMOVE AFTER BETA TO ALLOW PURCHASES
+            /**
+             * BETA ACCESS AND FREE CONTENT UNTIL RELEASE (around dec, 15, 2015)
+             */
+            tempContent.setHasAccess(1);
+            tempContent.setContentPrice(0);
+
+
+
+
+
+
+
+
+
             /**
              * Set content access. If free or the user is the publisher, user has access
              */
