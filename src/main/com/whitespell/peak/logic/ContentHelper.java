be84710ec07f1e886fa42c7d687727ef08a855ba
Pim De Witte <pim@whitespell.com>
2015-12-16 19:42:23 -0500

    test

--- src/main/com/whitespell/peak/logic/ContentHelper.java
+++ src/main/com/whitespell/peak/logic/ContentHelper.java
@@ -22,9 +22,10 @@ import java.sql.SQLException;
 public class ContentHelper {
 
     private static final String GET_CONTENT = "SELECT * FROM `content` as ct INNER JOIN `user` as ut ON ct.`user_id` = ut.`user_id` WHERE `content_id` = ? ";
+    private static final String GET_POPULAR_BUNDLE = "SELECT * FROM `content` as ct INNER JOIN `user` as ut ON ct.`user_id` = ut.`user_id` WHERE `category_id` = ? AND `processed` = 1 ORDER BY `content_id` DESC LIMIT 1";
 
 
-    public ContentObject getContentById(RequestObject context,int contentId, int requesterUserId) throws UnirestException {
+    public ContentObject getContentById(RequestObject context,int contentId, int requesterUserId) {
         final ContentObject[] content = {null};
 
         ContentWrapper contentWrapper = new ContentWrapper(context, requesterUserId);
@@ -33,6 +34,31 @@ public class ContentHelper {
 
             executor.execute(ps -> {
                 ps.setInt(1, contentId);
+
+                final ResultSet results = ps.executeQuery();
+
+                if (results.next()) {
+                    content[0] = contentWrapper.wrapContent(results);
+                }
+            });
+        } catch (SQLException e) {
+            Logging.log("High", e);
+            context.throwHttpError(this.getClass().getSimpleName(), StaticRules.ErrorCodes.UNKNOWN_SERVER_ISSUE);
+        }
+
+        return content[0];
+    }
+
+
+    public ContentObject getPopularBundleByCategoryId(RequestObject context,int categoryId, int requesterUserId) {
+        final ContentObject[] content = {null};
+
+        ContentWrapper contentWrapper = new ContentWrapper(context, requesterUserId);
+        try {
+            StatementExecutor executor = new StatementExecutor(GET_POPULAR_BUNDLE);
+
+            executor.execute(ps -> {
+                ps.setInt(1, categoryId);
 
                 final ResultSet results = ps.executeQuery();
 
