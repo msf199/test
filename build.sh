2ef8e6a68c1297c79cc8349535abf5437f25a5f6
Cory McAn <rulerfenrir@aim.com>
2015-12-18 15:28:29 -0500

    fix build, try again

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 OUTDIR=$1
 ROOT=$(cd $(dirname $0); pwd)
-CLASSPATH=$ROOT/lib/*:$ROOT/lib/jetty/*:$ROOT/lib/httpclient/*:$ROOT/lib/log4j/*:$ROOT/lib/aws/*:$ROOT/lib/aws/third-party/*:$ROOT/lib/joda-time-2.4/*:$ROOT/lib/androidPublisher/*
+CLASSPATH=$ROOT/lib/*:$ROOT/lib/jetty/*:$ROOT/lib/httpclient/*:$ROOT/lib/log4j/*:$ROOT/lib/aws/*:$ROOT/lib/joda-time-2.4/*:$ROOT/lib/androidPublisher/*
 SRCDIR=$ROOT/src
 
 if [ -z "$OUTDIR" ]; then
