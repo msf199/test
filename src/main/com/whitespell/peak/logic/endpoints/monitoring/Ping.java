37bfaf44f92a2b895135c1b31da6e1cc622a5f67
Pim De Witte <pim@whitespell.com>
2015-12-18 16:09:33 -0500

    rev twil

--- src/main/com/whitespell/peak/logic/endpoints/monitoring/Ping.java
+++ src/main/com/whitespell/peak/logic/endpoints/monitoring/Ping.java
@@ -1,20 +1,10 @@
 package main.com.whitespell.peak.logic.endpoints.monitoring;
 
 import com.google.gson.Gson;
-import com.twilio.sdk.TwilioRestClient;
-import com.twilio.sdk.TwilioRestException;
-import com.twilio.sdk.resource.factory.MessageFactory;
-import com.twilio.sdk.resource.instance.Account;
-import com.twilio.sdk.resource.instance.Message;
 import main.com.whitespell.peak.logic.EndpointHandler;
 import main.com.whitespell.peak.logic.RequestObject;
-import main.com.whitespell.peak.logic.logging.Logging;
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Pim de Witte(wwadewitte), Whitespell LLC
